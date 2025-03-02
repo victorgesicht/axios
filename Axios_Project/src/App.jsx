602620081a7975b2e86d67b204709913c184d2e9
Victorgesicht <victorwachira100@gmail.com>
2025-03-02 23:03:07 +0300

    axios Test

---
+++ Axios_Project/src/App.jsx
@@ -0,0 +1,10 @@
+import ApiScraping from "./components/apiScraping"
+function App() {
+  return (
+  <ApiScraping />
+  
+  )   
+}
+
+
+export default App
---
+++ Axios_Project/src/components/apiScraping.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const ApiScraping = () => {
+    const apiurl='https://jsonplaceholder.typicode.com/users'
+    const [data, setData]=useState([])
+
+    useEffect(() => {
+        axios.get(apiurl)
+        .then((response) => {
+            setData(response.data)
+            console.log(response.data)
+        })
+        .catch((error) => console.error('Error fetching data:', error))
+    }, [])
+
+  return (
+    <div>
+        
+        <h2 className='font-mono bg-slate-500'>SANTA'S NAUGHTY LIST</h2>
+        {data.map((item) => {
+            return (
+                <div key={item.id}>
+                    <h1 className='font-bold'>{item.name}</h1>
+                    <h3 className='font-light'>{item.email}</h3>
+                    <h4>{item.phone}</h4>
+                </div>                                  
+            )
+        })}
+    </div>
+  )
+}  
+
+
+export default ApiScraping
